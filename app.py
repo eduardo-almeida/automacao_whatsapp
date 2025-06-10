72d195645ea3d97b5f820e0ffda4512e5da7ea62
Eduardo Almeida <eduardoeandroid@gmail.com>
2025-06-10 14:41:21 -0300

    groq

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request
 from evolution_api import send_whatsapp_message
+from bot.ai_bot import AIBot
 # from message_buffer import buffer_message
 
-
 app = FastAPI()
+ai_bot = AIBot()
 
 @app.post('/webhook')
 async def webhook(request: Request):
@@ -16,10 +17,10 @@ async def webhook(request: Request):
         #     chat_id=chat_id,
         #     message=message,
         # )
+        response = ai_bot.invoke(question=message)
         send_whatsapp_message(
             number=chat_id,
-            text=f'O texto é \n{message}\n\n Obrigado por enviar a mensagem! ',
+            text=response,
         )
-        print("Ola Mundo")
 
     return {'status': 'ok'}
